3a69f196b1d13ebc72f7442c60509f0d4233f918
Andrii Gnedash <andrii.gnedash@ringcentral.com>
2024-05-23 15:05:44 +0200

    help added

--- src/nestor/__main__.py
+++ src/nestor/__main__.py
@@ -15,6 +15,37 @@ def parse_input(user_input: str) -> Tuple[str, List[str]]:
     args = parts[1:]
     return cmd, *args
 
+def help(command=None):
+    commands = {
+        "phone": "Show phone number by contact name. Example: phone <name>",
+        "add-contact": "Add a contact. Example: add-contact <name> <phone>",
+        "edit-contact": "Edit a contact. Example: edit-contact <name> <new phone>",
+        "delete-contact": "Delete a contact. Example: delete-contact <name>",
+        "show-birthday": "Show birthday by contact name. Example: show-birthday <name>",
+        "add-birthday": "Add a birthday. Example: add-birthday <name> <date>",
+        "birthdays": "Show upcoming birthdays. Example: birthdays <number of days>",
+        "add-email": "Add an email. Example: add-email <name> <email>",
+        "edit-email": "Edit an email. Example: edit-email <name> <new email>",
+        "show-email": "Show email by contact name. Example: show-email <name>",
+        "delete-email": "Delete an email. Example: delete-email <name>",
+        "add-address": "Add an address. Example: add-address <name> <street> <city> <state> <zip_code> <country>",
+        "edit-address": "Edit an address. Example: edit-address <name> <new street> <new city> <new state> <new zip_code> <new country>",
+        "contacts": "Show all contacts. Example: contacts",
+        "search-contacts": "Search contacts. Example: search-contacts <search string>"
+    }
+
+    if command:
+        if command in commands:
+            help_message = f"{command}: {commands[command]}\n"
+        else:
+            help_message = f"No help available for {command}\n"
+    else:
+        help_message = "Available commands:\n"
+        for cmd, description in commands.items():
+            help_message += f"{cmd}: {description}\n"
+
+    return Colorizer.info(help_message)
+
 
 def main():
     # load ContactsBook and initialize contacts handler
@@ -45,8 +76,10 @@ def main():
         elif command == "hello":
             ui.output(Colorizer.highlight("How can I help you?"))
         elif command == "help":
-            #todo: Implement
-            pass
+            if args:
+                ui.output(help(args[0]))
+            else:
+                ui.output(help())
         elif command in ContactsHandler.get_available_commands():
             ui.output(contacts_handler.handle(command, *args))
         elif command in NotesHandler.get_available_commands():
